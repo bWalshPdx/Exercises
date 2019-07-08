4ab4bd722020bdd3645d6ba577d987f1ac13f456
Brian.Walsh <databasebrian@gmail.com>
2019-07-08 09:16:52 -0700

    retouted to my main fizzubzz component

--- Angular/FizzBuzz/FizzBuzz/src/app/app.component.ts
+++ Angular/FizzBuzz/FizzBuzz/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './main/main.component.html',
+  styleUrls: ['./main/main.component.scss']
 })
 export class AppComponent {
   title = 'FizzBuzz';
