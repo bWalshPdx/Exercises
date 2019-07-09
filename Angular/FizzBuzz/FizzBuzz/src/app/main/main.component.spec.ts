d35a4a7e116a84cc915f1a21c0b99e5dd3e8f17f
Brian.Walsh <databasebrian@gmail.com>
2019-07-08 17:40:18 -0700

    Setting up the template to fail for the first real fizzBuzz test

--- Angular/FizzBuzz/FizzBuzz/src/app/main/main.component.spec.ts
+++ Angular/FizzBuzz/FizzBuzz/src/app/main/main.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainComponent } from './main.component';
+import { DebugElement } from '@angular/core';
 
 describe('MainComponent', () => {
   let component: MainComponent;
@@ -26,4 +27,14 @@ describe('MainComponent', () => {
     const selectedThing = compiled.querySelector('h1').textContent;
     expect(selectedThing).toContain('FizzBuzz Kata');
   });
+
+  it('should render a collction of 1 through 5', () => {
+    const fixture: ComponentFixture<MainComponent> = TestBed.createComponent(MainComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    const selectedThing = compiled.querySelector('#output').textContent;
+    console.log(selectedThing);
+    expect(selectedThing).toContain('12Fizz4Buzz');
+  });
+
 });
