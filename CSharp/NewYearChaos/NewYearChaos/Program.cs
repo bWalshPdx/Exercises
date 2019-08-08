1f1be30580bc577a7eadf02fda8327618252f57c
Brian.Walsh <databasebrian@gmail.com>
2019-08-08 09:47:34 -0700

    added example from hackerrank

--- CSharp/NewYearChaos/NewYearChaos/Program.cs
+++ CSharp/NewYearChaos/NewYearChaos/Program.cs
@@ -1,15 +1,56 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace NewYearChaos
+class Solution
 {
-    class Program
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q)
     {
-        static void Main(string[] args)
+        New_Year_Chaos mySolution = new New_Year_Chaos();
+
+        var output = mySolution.Solution(q);
+
+        foreach (string i in output)
         {
+            Console.WriteLine(i);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] q = Array.ConvertAll(Console.ReadLine().Split(' '), qTemp => Convert.ToInt32(qTemp));
+            minimumBribes(q);
+        }
+    }
+    public class New_Year_Chaos
+    {
+        public string[] Solution(int[] input)
+        {
+            Console.WriteLine("This was hit");
+
+            return new string[]
+            {
+                "3",
+                "Too chaotic"
+            };
         }
     }
 }
