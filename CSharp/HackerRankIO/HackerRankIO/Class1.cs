14dc877465b50763242977ff5bdffa85c49fe263
Brian Walsh <bwalshpdx@gmail.com>
2020-07-13 08:40:56 -0700

    MadeSimple HackerRank IO File

---
+++ CSharp/HackerRankIO/HackerRankIO/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRankIO
+{
+    public static class HackerRankIo
+    {
+        public static List<string> ReceiveInput()
+        { 
+            int elementsInputted = Int32.Parse(System.Console.ReadLine());
+
+            List<string> input = new List<string>();
+
+            for (int i = 0; i < elementsInputted; i++)
+            {
+                input.Add(System.Console.ReadLine());
+            }
+
+            return input;
+        }
+
+        public static void ReturnOutput(List<string> output)
+        {
+            foreach (var currentOutput in output)
+            {
+                System.Console.WriteLine(currentOutput);
+            }
+        }
+    }
+}
