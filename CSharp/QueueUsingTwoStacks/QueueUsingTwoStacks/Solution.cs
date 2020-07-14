66963d09513ab74b102b2f6a00340c01a0c51316
Brian Walsh <bwalshpdx@gmail.com>
2020-07-14 08:42:53 -0700

    Added Enqueue

--- CSharp/QueueUsingTwoStacks/QueueUsingTwoStacks/Solution.cs
+++ CSharp/QueueUsingTwoStacks/QueueUsingTwoStacks/Solution.cs
@@ -12,10 +12,31 @@ namespace QueueUsingTwoStacks
         private string Value { get; set; }
         private Custom_Queue Next { get; set; }
 
+        public Custom_Queue()
+        {
+            
+        }
+
         public Custom_Queue(string value)
         {
             Value = value;
         }
+
+        public void Enqueue(string value)
+        {
+            string output = Value;
+
+            if (String.IsNullOrEmpty(Value))
+            {
+                Value = value;
+            }
+            else
+            {
+                Next = new Custom_Queue();
+                Next.Enqueue(value);
+            }
+        }
+
 
         public string Dequeue()
         {
--- CSharp/QueueUsingTwoStacks/Solution-Tests/Solution_Tests.cs
+++ CSharp/QueueUsingTwoStacks/Solution-Tests/Solution_Tests.cs
@@ -8,11 +8,25 @@ namespace Solution_Tests
     public class Solution_Tests
     {
         [Fact]
-        public void Initialize_GivengARecievedValue_ShouldAddValueToTheStack()
+        public void Initialize_GivenARecievedValue_ShouldAddValueToTheStack()
         {
             string testValue = "0";
 
             Custom_Queue queue = new Custom_Queue(testValue);
+
+            string value = queue.Dequeue();
+
+            value.Should().BeEquivalentTo(testValue);
+        }
+
+        [Fact]
+        public void Enqueue_GivenARecievedValue_ShouldAddValueToTheStack()
+        {
+            string testValue = "0";
+
+            Custom_Queue queue = new Custom_Queue();
+
+            queue.Enqueue(testValue);
 
             string value = queue.Dequeue();
 
