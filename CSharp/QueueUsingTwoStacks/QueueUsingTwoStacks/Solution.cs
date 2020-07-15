3eca376a8970a19a4b8906e016fcab78053a2fe7
Brian Walsh <bwalshpdx@gmail.com>
2020-07-15 08:48:19 -0700

    Fiddling with valuetuple

--- CSharp/QueueUsingTwoStacks/QueueUsingTwoStacks/Solution.cs
+++ CSharp/QueueUsingTwoStacks/QueueUsingTwoStacks/Solution.cs
@@ -4,34 +4,25 @@ namespace QueueUsingTwoStacks
 {
     public class Solution
     {
-
+        
     }
+
+    
 
     public class Custom_Queue
     {
-        private string Value { get; set; }
-        private Custom_Queue Next { get; set; }
+        private ValueTuple<string, ValueTuple> Head { get; set; }
 
-        public Custom_Queue()
-        {
-            
-        }
+        private ValueTuple<string, ValueTuple> Tail { get; set; }
 
-        public Custom_Queue(string value)
-        {
-            Value = value;
-        }
 
         public void Enqueue(string value)
         {
-            
-            if (!String.IsNullOrEmpty(Value))
-            {
-                Next = new Custom_Queue();
-                Next.Enqueue(Value);
-            }
 
-            Value = value;
+            if(!String.IsNullOrEmpty(Head.Item1))
+            {
+                Head = (value, Head);
+            }
         }
 
 
--- CSharp/QueueUsingTwoStacks/README.md
+++ CSharp/QueueUsingTwoStacks/README.md
@@ -4,3 +4,5 @@ https://www.hackerrank.com/challenges/queue-using-two-stacks/problem
 --NA 2020.07.13.09.05.41AM: Work on making a datastructure that can enqueue a element
 
 <NA> 2020.07.14.09.01.30AM: I need to make the head the tail something list Next = this (But without the reference issues) (Some sort of deep copy?)
+
+<NA> 2020.07.15.08.47.15AM: Fiddling with value types via a 'valuetuple' and adding the head to the tail.
