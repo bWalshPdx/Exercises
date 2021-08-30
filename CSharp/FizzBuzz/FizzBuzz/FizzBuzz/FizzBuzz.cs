9664a53a32640b567f734e8a0b872ebe83dc4793
Brian Walsh <bwalsh@ruby.com>
2021-08-29 19:16:54 -0700

    checking in fizzbuzz, don't know why I wrote it

--- CSharp/FizzBuzz/FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ CSharp/FizzBuzz/FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -5,54 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-
-
-
-
 namespace FizzBuzz
 {
-    public static class Solution
-    {
-        static void Main(String[] args)
-        {
-            FizzBuzz fizzBuzz = new FizzBuzz();
-
-            Console.Write(fizzBuzz.GetCollection(100));
-        }
-    }
-
     public class FizzBuzz
     {
-        public string Convert(int input)
+        public string GetOutput(List<int> input)
         {
-            string output = "";
-
-            if (input % 3 == 0)
-            {
-                output += "Fizz";
-            }
-
-            if (input % 5 == 0)
-            {
-                output += "Buzz";
-            }
-
-            if (output == "")
-            {
-                output = input.ToString();
-            }
-
-            return output;
-        }
-
-        public string GetCollection(int limit)
-        {
-            var range = Enumerable.Range(1, limit).ToList();
             StringBuilder sb = new StringBuilder();
+            
+            foreach (var i in input)
+            {
+                string current;
+                if (i % 3 == 0)
+                {
+                    current = "Fizz ";
+                }
+                else
+                {
+                    current = i + " ";
+                }
+                sb.Append(current);
+            }
+            
+            string output = sb.ToString();
 
-            range.ForEach(e => sb.AppendLine(Convert(e)));
-
-            return sb.ToString();
+            if (output.Last() == ' ')
+            {
+                output = output.Substring(0, output.Length - 1);
+            }
+            
+            return output;
         }
     }
 }
--- CSharp/FizzBuzz/FizzBuzz/FizzBuzz/FizzBuzz_Tests.cs
+++ CSharp/FizzBuzz/FizzBuzz/FizzBuzz/FizzBuzz_Tests.cs
@@ -8,87 +8,40 @@ using Xunit.Sdk;
 
 namespace FizzBuzz
 {
-
-    [Collection("Root Test")]
     public class FizzBuzz_Tests
     {
-        [Theory]
-        [InlineData(1, "1")]
-        [InlineData(2, "2")]
-        [InlineData(4, "4")]
-        public void Convert_ShouldReturnItself(int input, string expectedOutput)
-        {
-            FizzBuzz fizzBuzz = new FizzBuzz();
-            
-            string output = fizzBuzz.Convert(input);
-
-            Assert.Equal(expectedOutput, output);
-
-        }
-
-
-        [Theory]
-        [InlineData(3, "Fizz")]
-        public void Convert_ShouldReturnFizz(int input, string expectedOutput)
-        {
-            FizzBuzz fizzBuzz = new FizzBuzz();
-
-            string output = fizzBuzz.Convert(input);
-
-            Assert.Equal(expectedOutput, output);
-        }
-
-        [Theory]
-        [InlineData(5, "Buzz")]
-        public void Convert_ShouldReturnBuzz(int input, string expectedOutput)
-        {
-            FizzBuzz fizzBuzz = new FizzBuzz();
-
-            string output = fizzBuzz.Convert(input);
-
-            Assert.Equal(expectedOutput, output);
-        }
-
-        [Theory]
-        [InlineData(15, "FizzBuzz")]
-        public void Convert_ShouldReturnFizzBuzz(int input, string expectedOutput)
-        {
-            FizzBuzz fizzBuzz = new FizzBuzz();
-
-            string output = fizzBuzz.Convert(input);
-
-            Assert.Equal(expectedOutput, output);
-        }
-
         [Fact]
-        public void Convert_ShouldReturnCorrectCollection()
+        public async Task GetOutput_Given3Inputs_ReturnsFizz()
         {
             FizzBuzz fizzBuzz = new FizzBuzz();
-
-            int limit = 15;
-
-            string expectedOutput = @"1
-2
-Fizz
-4
-Buzz
-Fizz
-7
-8
-Fizz
-Buzz
-11
-Fizz
-13
-14
-FizzBuzz" + Environment.NewLine;
-
-
-            string output = fizzBuzz.GetCollection(limit);
-
-            Assert.Equal(expectedOutput, output);
+            List<int> input = new List<int>() {1, 2, 3};
+            
+            string output =  fizzBuzz.GetOutput(input);
+            
+            Assert.Equal("1 2 Fizz", output);
         }
-
-
+        
+        [Fact]
+        public async Task GetOutput_Given4Inputs_ReturnsFizz()
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            List<int> input = new List<int>() {1, 2, 3, 4};
+            
+            string output =  fizzBuzz.GetOutput(input);
+            
+            Assert.True(output == "1 2 Fizz 4");
+        }
+        
+        [Fact]
+        public async Task GetOutput_Given4Inputs_ReturnsFizz()
+        { 
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            List<int> input = new List<int>() {1, 2, 3, 4};
+            
+            string output =  fizzBuzz.GetOutput(input);
+            
+            Assert.True(output == "1 2 Fizz 4");
+        }
     }
+    
 }
