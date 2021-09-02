4bcd6bcd21337cf84ef9b4efde5af56c7f1024da
Brian Walsh <bwalsh@ruby.com>
2021-09-02 15:24:40 -0700

    completed LC-TwoSums

--- Python/LeetCode/TwoSums/solution.py
+++ Python/LeetCode/TwoSums/solution.py
@@ -1,7 +1,11 @@
-import string
+from typing import List
 
 
 class Solution:
-    def twoSum(self) -> string:
-        return "value"
-    
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for outerIndex, outer in enumerate(nums):
+            for innerIndex, inner in enumerate(nums,):
+                if innerIndex <= outerIndex:
+                    continue
+                if (outer + inner) == target:
+                    return [outerIndex, innerIndex]
--- Python/LeetCode/TwoSums/test_solution.py
+++ Python/LeetCode/TwoSums/test_solution.py
@@ -1,12 +1,29 @@
 import unittest
-from itertools import count
+from typing import List
 
 from solution import Solution
 
 if __name__ == '__main__':
     pass
 
+
 class TestSolution(unittest.TestCase):
-    def test_two_sum_returnsValue(self):
+    def test_Fact1(self):
         solution = Solution()
-        self.assertTrue(len(solution.twoSum()) > 0)
+        output: List[int] = solution.twoSum([2, 7, 11, 15], 9)
+        self.assertListEqual(output, [0, 1])
+
+    def test_Fact2(self):
+        solution = Solution()
+        output: List[int] = solution.twoSum([3, 3], 6)
+        self.assertListEqual(output, [0, 1])
+
+    def test_Fact3(self):
+        solution = Solution()
+        output: List[int] = solution.twoSum([3, 2, 4], 6)
+        self.assertListEqual(output, [1, 2])
+
+    def test_Fact4(self):
+        solution = Solution()
+        output: List[int] = solution.twoSum([2, 5, 5, 11], 10)
+        self.assertListEqual(output, [1, 2])
