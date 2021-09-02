57ef8376d526d0e403a19ca01dfc3947a9ecaaa0
Brian Walsh <bwalsh@ruby.com>
2021-09-02 16:23:35 -0700

    first failing test

---
+++ Python/LeetCode/Google-Interview/Interview-Process/Unique-Email-Address/UniqueEmail-tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+if __name__ == '__main__':
+    pass
+
+
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        pass
+
+
+class TestUniqueEmails(unittest.TestCase):
+
+    def test_Work(self):
+        solution = Solution()
+        emails = ["test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"]
+        output: int = solution.numUniqueEmails(emails)
+        self.assertEqual(3, output)
