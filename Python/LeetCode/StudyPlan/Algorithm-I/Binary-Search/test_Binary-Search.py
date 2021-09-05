0b19da503c20ab8a4ec57de5b370d8b1b48158b6
Brian Walsh <bwalsh@ruby.com>
2021-09-05 10:38:16 -0700

    initial tests are passing

--- Python/LeetCode/StudyPlan/Algorithm-I/Binary-Search/test_Binary-Search.py
+++ Python/LeetCode/StudyPlan/Algorithm-I/Binary-Search/test_Binary-Search.py
@@ -8,19 +8,45 @@ if __name__ == '__main__':
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        halfIndex: int = math.floor(len(nums) / 2 -1)
+        nextMove:int = len(nums) // 2
+        currentIndex:int = nextMove
+        while True:
 
-        if nums[halfIndex] == target:
-            return halfIndex
+            if nums[currentIndex] == target:
+                return currentIndex
+            elif nextMove == 0:
+                return -1
+
+            nextMove: int = nextMove // 2
+            if nextMove == 0:
+                nextMove = 1
+
+            if nums[currentIndex] > target:
+                currentIndex = currentIndex - nextMove
+            else:
+                currentIndex = currentIndex + nextMove
+
+            if (nextMove == 1):
+                nextMove = 0
 
 
 class TestSolution(unittest.TestCase):
+    #@unittest.skip("reason for skipping")
+    def test_FromLeetCode(self):
+        solution = Solution()
+        self.assertEqual(solution.search([-1, 0, 3, 5, 9, 12], 9), 4)
+        self.assertEqual(solution.search([-1, 0, 3, 5, 9, 12], 2), -1)
 
-    # def test_FromLeetCode(self):
-    #     solution = Solution()
-    #     self.assertEqual(solution.search([-1, 0, 3, 5, 9, 12], 9), 4)
-    #     self.assertEqual(solution.search([-1, 0, 3, 5, 9, 12], 2), -1)
-
+    #@unittest.skip("reason for skipping")
     def test_findAtFirstTry(self):
         solution = Solution()
-        self.assertEqual(solution.search([3], 3), 0)
+        self.assertEqual(0, solution.search([3], 3))
+        self.assertEqual(1, solution.search([1, 2, 3], 2))
+        self.assertEqual(2, solution.search([1, 2, 3, 4, 5], 3))
+
+
+    #@unittest.skip("reason for skipping")
+    def test_findAtSecondTry(self):
+        solution = Solution()
+        self.assertEqual(0, solution.search([1, 2, 3], 1))
+        self.assertEqual(2, solution.search([1, 2, 3], 3))
