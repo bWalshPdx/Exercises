a6c7910672d5c38d13d776a649aa6fe7f2acc3c1
Brian Walsh <bwalsh@ruby.com>
2021-09-06 15:09:22 -0700

    Added blank solution for FirstBadVersion

---
+++ Python/LeetCode/StudyPlan/Algorithm-I/First-Bad-Version/test_First-Bad-Version.py
@@ -0,0 +1,19 @@
+import unittest
+
+if __name__ == '__main__':
+    pass
+
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+class TestSolution(unittest.TestCase):
+
+    #@unittest.skip()
+    def test_Fact1(self):
+        solution = Solution()
+        self.assertEqual(1, 2)
