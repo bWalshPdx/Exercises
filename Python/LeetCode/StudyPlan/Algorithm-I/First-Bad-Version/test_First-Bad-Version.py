9e35c028edef2a6e58b2ba0e41612ed58307a82e
Brian Walsh <bwalsh@ruby.com>
2021-09-09 17:57:24 -0700

    still working on out of index errors

--- Python/LeetCode/StudyPlan/Algorithm-I/First-Bad-Version/test_First-Bad-Version.py
+++ Python/LeetCode/StudyPlan/Algorithm-I/First-Bad-Version/test_First-Bad-Version.py
@@ -11,23 +11,28 @@ def isBadVersion(n: int) -> bool:
     return n == badVersionForTest
 
 class Solution:
-    def firstBadVersion(self, n: int) -> int:
-        input: List[int] = range(0, n)
-        self.getNextBadVersion(input, 0, len(input))
+    def firstBadVersion(self, highest_version_number: int) -> int:
+        new_input: List[int] = list(range(highest_version_number + 1))
+        return self.getNextBadVersion(new_input, 0, len(new_input))
 
-    def getNextBadVersion(self, input: List[int], left: int, right: int) -> int:
-        mid = left + right // 2
+    def getNextBadVersion(self, current_input: List[int], left: int, right: int) -> int:
+        if left > right:
+            return -1
+
+        mid = (left + right) // 2
         left_result = -1
         right_result = -1
-        is_bad = isBadVersion(input[mid])
+        is_bad = isBadVersion(current_input[mid])
+
+
 
         if is_bad:
-            left_result = self.getNextBadVersion(List[int], left, mid - 1) # Go Left to find earlier bad version
+            left_result = self.getNextBadVersion(current_input, left, mid - 1) #Go Left to find earlier bad version
         else:
-            right_result = self.getNextBadVersion(List[int], mid + 1, right)  # Go Right to find later bad version
+            right_result = self.getNextBadVersion(current_input, mid + 1, right) #Go Right to find later bad version
 
         if is_bad and left_result == -1:
-            return input[mid]
+            return current_input[mid]
         else:
             return right_result
 
@@ -36,10 +41,17 @@ class Solution:
 class TestSolution(unittest.TestCase):
 
     # @unittest.skip()
-    def test_Fact1(self):
+    def test_ListOf0(self):
         solution = Solution()
 
         global badVersionForTest
-        badVersionForTest = 1
-        self.assertEqual(1, solution.firstBadVersion(1))
-        #self.assertEqual(1, solution.firstBadVersion(2))
+        badVersionForTest = 0
+        self.assertEqual(badVersionForTest, solution.firstBadVersion(0))
+
+
+    def test_ListOf2(self):
+        solution = Solution()
+
+        global badVersionForTest
+        badVersionForTest = 0
+        self.assertEqual(badVersionForTest, solution.firstBadVersion(1))
