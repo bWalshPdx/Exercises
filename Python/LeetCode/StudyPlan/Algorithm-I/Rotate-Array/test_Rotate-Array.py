a5a803b330ecb634a67b24537ca59e40f051fd4e
Brian Walsh <databasebrian@gmail.com>
2021-09-30 12:55:11 -0700

    Started: Rotate-Array problem

---
+++ Python/LeetCode/StudyPlan/Algorithm-I/Rotate-Array/test_Rotate-Array.py
@@ -0,0 +1,17 @@
+import unittest
+
+if __name__ == '__main__':
+    pass
+
+
+class Solution(object):
+    def main(self, input) -> None:
+        pass
+
+
+class TestSolution(unittest.TestCase):
+
+    #@unittest.skip()
+    def test_Fact1(self):
+        solution = Solution()
+        self.assertEqual(1, 2)
