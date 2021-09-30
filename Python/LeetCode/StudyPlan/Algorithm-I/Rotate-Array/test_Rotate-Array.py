ec75d94333cad02b8df5202fb7dd4b5312aecb0a
Brian Walsh <databasebrian@gmail.com>
2021-09-30 13:03:50 -0700

    BROKEN: Not rotating yet

--- Python/LeetCode/StudyPlan/Algorithm-I/Rotate-Array/test_Rotate-Array.py
+++ Python/LeetCode/StudyPlan/Algorithm-I/Rotate-Array/test_Rotate-Array.py
@@ -1,17 +1,29 @@
 import unittest
+from typing import List
+
 
 if __name__ == '__main__':
     pass
 
 
-class Solution(object):
-    def main(self, input) -> None:
-        pass
-
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        return nums
+        """
+        Do not return anything, modify nums in-place instead.
+        """
 
 class TestSolution(unittest.TestCase):
 
     #@unittest.skip()
-    def test_Fact1(self):
+    def test_EmptyListReturnsEmptyList(self):
         solution = Solution()
-        self.assertEqual(1, 2)
+        input = []
+        solution.rotate(input, 0)
+        self.assertEqual([], input)
+
+    def test_TwoElementsRotateOne(self):
+        solution = Solution()
+        input = [1, 2]
+        solution.rotate(input, 1)
+        self.assertEqual([2, 1], input)
