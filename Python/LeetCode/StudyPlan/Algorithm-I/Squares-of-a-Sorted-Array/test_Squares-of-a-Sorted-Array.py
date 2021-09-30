05c49cd79d80a6d07cc50a1aed834c24c8509661
Brian Walsh <databasebrian@gmail.com>
2021-09-29 20:41:05 -0700

    BROKEN: Squares are done, but sorting is next

--- Python/LeetCode/StudyPlan/Algorithm-I/Squares-of-a-Sorted-Array/test_Squares-of-a-Sorted-Array.py
+++ Python/LeetCode/StudyPlan/Algorithm-I/Squares-of-a-Sorted-Array/test_Squares-of-a-Sorted-Array.py
@@ -7,12 +7,31 @@ if __name__ == '__main__':
 
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        return []
+        output = []
+        if len(nums) == 0:
+            return nums
 
+        for i in nums:
+            output.append(i * i)
+
+        return output
 
 class TestSolution(unittest.TestCase):
 
     #@unittest.skip()
-    def test_Fact1(self):
+    def test_EmptyReturnsEmpty(self):
         solution = Solution()
         self.assertEqual([], solution.sortedSquares([]))
+
+
+    def test_SingleElementReturnsSquare1(self):
+        solution = Solution()
+        self.assertEqual([1], solution.sortedSquares([1]))
+
+    def test_SingleElementReturnsSquare2(self):
+        solution = Solution()
+        self.assertEqual([100], solution.sortedSquares([10]))
+
+    def test_SingleElementReturnsSquare2(self):
+        solution = Solution()
+        self.assertEqual([1, 100], solution.sortedSquares([10, 1]))
