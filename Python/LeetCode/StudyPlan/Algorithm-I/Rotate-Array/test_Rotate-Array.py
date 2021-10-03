7dbc24fa822aaa3f1903ca5e52ea7ad37505d51f
Brian Walsh <databasebrian@gmail.com>
2021-10-02 19:01:03 -0700

    first LC example passed

--- Python/LeetCode/StudyPlan/Algorithm-I/Rotate-Array/test_Rotate-Array.py
+++ Python/LeetCode/StudyPlan/Algorithm-I/Rotate-Array/test_Rotate-Array.py
@@ -13,20 +13,27 @@ class Solution:
         if shift == 0:
             return current_index
 
-        #newIndex: int = current_index + shift
         new_index = current_index + shift
 
         if new_index <= (array_length - 1):
             return new_index
 
-        return (current_index + shift) % array_length
+        new_index = (current_index + shift) % array_length
 
+        return new_index
 
     def rotate(self, nums: List[int], k: int) -> None:
-        return nums
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        output = [None] * len(nums)
+
+        if len(nums) <= 1:
+            return nums
+
+        for index, item in enumerate(nums):
+            new_index = self.get_new_index(len(nums), index, k)
+            output[new_index] = item
+
+        nums[:] = output
+
 
 class TestSolution(unittest.TestCase):
 
@@ -64,9 +71,22 @@ class TestSolution(unittest.TestCase):
         shift = 1
         self.assertEqual(0, solution.get_new_index(array_length, current_index, shift))
 
+    def test_newIndex_Example4(self):
+        solution = Solution()
+        array_length = 4
+        current_index = 2
+        shift = 4
+        self.assertEqual(2, solution.get_new_index(array_length, current_index, shift))
 
-    # def test_TwoElementsRotateOne(self):
-    #     solution = Solution()
-    #     input = [1, 2]
-    #     solution.rotate(input, 1)
-    #     self.assertEqual([2, 1], input)
+    def test_newIndex_Example5(self):
+        solution = Solution()
+        array_length = 5
+        current_index = 0
+        shift = 7
+        self.assertEqual(2, solution.get_new_index(array_length, current_index, shift))
+
+    def test_newIndex_LC_Example1(self):
+        solution = Solution()
+        input = [1, 2, 3, 4, 5, 6, 7]
+        solution.rotate(input, 3)
+        self.assertEqual([5, 6, 7, 1, 2, 3, 4], input)
