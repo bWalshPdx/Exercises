5c360d5ed1dbcd4877ec84f0592ae93e394c6657
Brian Walsh <bwalsh@ruby.com>
2021-10-11 10:03:54 -0700

    working on the single word reversal of this problem

--- Python/LeetCode/StudyPlan/Algorithm-I/Reverse-Words-in-a-String-III/test_Reverse-Words-in-a-String-III.py
+++ Python/LeetCode/StudyPlan/Algorithm-I/Reverse-Words-in-a-String-III/test_Reverse-Words-in-a-String-III.py
@@ -3,10 +3,21 @@ import unittest
 if __name__ == '__main__':
     pass
 
+# <NA> 2021.10.11.10.02.54AM: Working on reversing a single word just like the previous problem, then move up to multiple words:
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        start: int = 0
+        end: int = len(s) - 1
 
-class Solution(object):
-    def main(self, input) -> None:
-        pass
+        while True:
+            start = start + 1
+            end = end -1
+
+
+
+
+
+        return "i"
 
 
 class TestSolution(unittest.TestCase):
@@ -14,4 +25,9 @@ class TestSolution(unittest.TestCase):
     #@unittest.skip()
     def test_Fact1(self):
         solution = Solution()
-        self.assertEqual(1, 2)
+        self.assertEqual("i", solution.reverseWords("i"))
+
+
+    def test_Fact1(self):
+        solution = Solution()
+        self.assertEqual("ih", solution.reverseWords("hi"))
