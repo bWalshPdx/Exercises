8618c9c0694bb0b2478f4401a69e25373d28a904
Brian Walsh <bwalsh@ruby.com>
2021-10-14 09:57:16 -0700

    reverse-words-in-a-string-iii accepted

--- Python/LeetCode/StudyPlan/Algorithm-I/Reverse-Words-in-a-String-III/test_Reverse-Words-in-a-String-III.py
+++ Python/LeetCode/StudyPlan/Algorithm-I/Reverse-Words-in-a-String-III/test_Reverse-Words-in-a-String-III.py
@@ -3,9 +3,25 @@ import unittest
 if __name__ == '__main__':
     pass
 
-# <NA> 2021.10.11.10.02.54AM: Working on reversing a single word just like the previous problem, then move up to multiple words:
+# <NA> 2021.10.13.10.48.16AM: Working on 'append' or something similar that will join the reversed sentence
 class Solution:
+
     def reverseWords(self, s: str) -> str:
+        sentenceArr = s.split(' ')
+
+        i = 0
+        while True:
+            tempVal = len(sentenceArr) - 1
+            if tempVal < i:
+                break
+
+            sentenceArr[i] = self.reverseOneWord(sentenceArr[i])
+
+            i = i + 1
+
+        return " ".join(sentenceArr)
+
+    def reverseOneWord(self, s: str) -> str:
         sArr = list(s)
         start: int = 0
         end: int = len(s) - 1
@@ -42,3 +58,15 @@ class TestSolution(unittest.TestCase):
     def test_Fact3(self):
         solution = Solution()
         self.assertEqual("olleh", solution.reverseWords("hello"))
+
+    def test_TwoWordsAreReversed(self):
+        solution = Solution()
+        self.assertEqual("ih ih", solution.reverseWords("hi hi"))
+
+    def test_LCExample1(self):
+        solution = Solution()
+        self.assertEqual("s'teL ekat edoCteeL tsetnoc", solution.reverseWords("Let's take LeetCode contest"))
+
+    def test_LCExample2(self):
+        solution = Solution()
+        self.assertEqual("doG gniD", solution.reverseWords("God Ding"))
