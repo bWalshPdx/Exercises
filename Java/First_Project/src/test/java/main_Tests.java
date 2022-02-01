74a1b3b1140cee09377c72ac44ec6d0081d331c4
Brian Walsh <bwalsh@ruby.com>
2022-02-01 10:44:32 -0800

    changed first project to be maven. Able to run a test.

---
+++ Java/First_Project/src/test/java/main_Tests.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class main_Tests {
+
+    @Test
+    public void Bootstrap(){
+        Assert.assertTrue(true);
+    }
+    //Stopped at looking at hamcrest assertions:
+    //https://www.youtube.com/watch?v=qkblc5WRn-U&t=1633s
+    // @ 27:10
+}
