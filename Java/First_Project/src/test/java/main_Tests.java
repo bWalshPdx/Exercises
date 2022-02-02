3baef2afbcec41cea650e4636ba3ba9221bcab6c
Brian Walsh <bwalsh@ruby.com>
2022-02-02 10:49:27 -0800

    basic tests underway

--- Java/First_Project/src/test/java/main_Tests.java
+++ Java/First_Project/src/test/java/main_Tests.java
@@ -1,9 +1,12 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Collections.emptyList;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class main_Tests {
@@ -11,14 +14,24 @@ public class main_Tests {
     @Test
     public void factors(){
         assertThat(factorsOf(1)).isEqualTo(emptyList());
+        assertThat(factorsOf(2)).isEqualTo(Arrays.asList(2));
+        assertThat(factorsOf(3)).isEqualTo(Arrays.asList(3));
+        //assertThat(factorsOf(4)).isEqualTo(Arrays.asList(2, 2));
     }
 
     private List<Integer> factorsOf(int i) {
-        return emptyList();
+        var factors = new ArrayList<Integer>();
+        if(i > 1){
+            factors.add(i);
+        }
+
+
+
+        return factors;
     }
 
 
-    //Stopped at looking at hamcrest assertions:
+
     //https://www.youtube.com/watch?v=qkblc5WRn-U&t=1633s
     // @ 27:10
 
