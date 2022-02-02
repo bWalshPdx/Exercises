6d3b602345e4025712ef7a54a1a4d10c62922263
Brian Walsh <bwalsh@ruby.com>
2022-02-02 09:57:38 -0800

    First_Project:first test pases

--- Java/First_Project/src/test/java/main_Tests.java
+++ Java/First_Project/src/test/java/main_Tests.java
@@ -1,14 +1,23 @@
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.*;
 
 public class main_Tests {
 
     @Test
-    public void Bootstrap(){
-        assertThat("NotTrue").isEqualTo("True");
-
+    public void factors(){
+        assertThat(factorsOf(1)).isEqualTo(emptyList());
     }
+
+    private List<Integer> factorsOf(int i) {
+        return emptyList();
+    }
+
+
     //Stopped at looking at hamcrest assertions:
     //https://www.youtube.com/watch?v=qkblc5WRn-U&t=1633s
     // @ 27:10
