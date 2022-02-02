0e80a23f36763406116593fa849b3efdb5acb495
Brian Walsh <bwalshpdx@gmail.com>
2022-02-01 20:15:41 -0800

    added assertJ for a simple assertion

--- Java/First_Project/src/test/java/main_Tests.java
+++ Java/First_Project/src/test/java/main_Tests.java
@@ -1,13 +1,16 @@
 import org.junit.Assert;
 import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
 
 public class main_Tests {
 
     @Test
     public void Bootstrap(){
-        Assert.assertTrue(true);
+        assertThat("NotTrue").isEqualTo("True");
+
     }
     //Stopped at looking at hamcrest assertions:
     //https://www.youtube.com/watch?v=qkblc5WRn-U&t=1633s
     // @ 27:10
+
 }
