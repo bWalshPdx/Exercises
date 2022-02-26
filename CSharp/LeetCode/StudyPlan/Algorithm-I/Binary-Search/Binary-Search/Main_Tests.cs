213567836a6828a0520578896a7f193bca3dc81b
Brian Walsh <bwalsh@ruby.com>
2022-02-26 08:24:25 -0800

    Basic search complete

--- CSharp/LeetCode/StudyPlan/Algorithm-I/Binary-Search/Binary-Search/Main_Tests.cs
+++ CSharp/LeetCode/StudyPlan/Algorithm-I/Binary-Search/Binary-Search/Main_Tests.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using NUnit.Framework;
@@ -9,7 +10,15 @@ namespace Binary_Search
     public class Solution {
         public int Search(int[] nums, int target)
         {
-            return 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
     }
     
@@ -25,6 +34,30 @@ namespace Binary_Search
             int output = solution.Search(input, target);
 
             output.Should().Be(0);
+        }
+        
+        [Fact]
+        public async Task Search_GivenTwoElements_ShouldReturnCorrectIndex()
+        {
+            Solution solution = new Solution();
+            int[] input = new int[] {5,9};
+            int target = 9;
+
+            int output = solution.Search(input, target);
+
+            output.Should().Be(1);
+        }
+        
+        [Fact]
+        public async Task Search_GivenTargetNotFound_ShouldReturnNegativeOne()
+        {
+            Solution solution = new Solution();
+            int[] input = new int[] {5,9};
+            int target = 0;
+
+            int output = solution.Search(input, target);
+
+            output.Should().Be(-1);
         }
     }
 
