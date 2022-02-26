dfbb1e5d84daab0b8e1df5756386ea14de299487
Brian Walsh <bwalsh@ruby.com>
2022-02-26 08:04:37 -0800

    basic bootstrap working

--- CSharp/LeetCode/StudyPlan/Algorithm-I/Binary-Search/Binary-Search/Main_Tests.cs
+++ CSharp/LeetCode/StudyPlan/Algorithm-I/Binary-Search/Binary-Search/Main_Tests.cs
@@ -4,12 +4,22 @@ using Xunit;
 
 namespace Binary_Search
 {
-    public class MainTests
+    public class Solution {
+        public int Search(int[] nums, int target)
+        {
+            return 0;
+        }
+    }
+    
+    public class Solution_Tests
     {
         [Fact]
-        public async Task Bootstrap()
+        public async Task Search_Bootstrap()
         {
-            
+            Solution solution = new Solution();
+
+            solution.Search(new int[0], 0);
+
         }
     }
 
