69e7ecc7dc270d5095ec31fb7646bf72b34dc50a
Brian Walsh <bwalsh@ruby.com>
2022-03-03 10:20:24 -0800

    two solutions are passing

--- CSharp/LeetCode/StudyPlan/Microsoft/TwoSums/TwoSums/Solution.cs
+++ CSharp/LeetCode/StudyPlan/Microsoft/TwoSums/TwoSums/Solution.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Xunit;
 
 namespace TwoSums
@@ -7,7 +8,18 @@ namespace TwoSums
     public class Solution {
         public int[] TwoSum(int[] nums, int target)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new[] {i, j};
+                    }
+                }
+            }
+
+            throw new ArgumentException();
         }
     }
 
@@ -15,10 +27,31 @@ namespace TwoSums
     public class Solution_Tests
     {
         [Fact]
-        public async Task TwoSums_Bootstrap()
+        public async Task TwoSums_Fact1()
         {
             Solution solution = new Solution();
-            solution.TwoSum(new[] {0}, 0);
+            int[] input = new[] {3,2,4};
+            int target = 6;
+            int[] expectedOutput = new[] {1,2};
+            
+            int[] output = solution.TwoSum(input, target);
+
+            output.Should().BeEquivalentTo(expectedOutput);
         }
+        
+        
+        [Fact]
+        public async Task TwoSums_Fact2()
+        {
+            Solution solution = new Solution();
+            int[] input = new[] {2,7,11,15};
+            int target = 9;
+            int[] expectedOutput = new[] {0, 1};
+            
+            int[] output = solution.TwoSum(input, target);
+
+            output.Should().BeEquivalentTo(expectedOutput);
+        }
+        
     }
 }
