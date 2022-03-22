0dfead4d139a2b5cf1f07249f045a0999dcb6294
Brian Walsh <bwalsh@ruby.com>
2022-03-21 18:20:53 -0700

    found the first 3 problems with ATOI, but working on the fourth

--- CSharp/LeetCode/StudyPlan/Microsoft/ATOI/ATOI/Solution.cs
+++ CSharp/LeetCode/StudyPlan/Microsoft/ATOI/ATOI/Solution.cs
@@ -1,15 +1,52 @@
-﻿using FluentAssertions;
+﻿using System.Text;
+using FluentAssertions;
 using Xunit;
 
 namespace ATOI;
 
 public class Solution {
-    public int MyAtoi(string s)
+    public int MyAtoi(string input)
     {
-        string noWhiteSpace = String.Concat(s.Where(c => !Char.IsWhiteSpace(c)));
+        bool getIntegerChars = false;
+        StringBuilder sb = new StringBuilder();
         
-        return Int32.Parse(noWhiteSpace);
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (Char.IsWhiteSpace(input[i]))
+            {
+                getIntegerChars = false;
+
+                if (getIntegerChars)
+                {
+                    break;
+                }
+            }
+            else if (input[i] == '-')
+            {
+                if (getIntegerChars)
+                {
+                    getIntegerChars = false;
+                    break;
+                }
+                else
+                {
+                    getIntegerChars = true;
+                }
+            }
+            else if (Char.IsDigit(input[i]))
+            {
+                getIntegerChars = true;
+            }
+            
+            if(getIntegerChars)
+                sb.Append(input[i]);
+        }
+        
+        return Int32.Parse(sb.ToString());
     }
+
+    
 }
 
 
@@ -49,4 +86,18 @@ public class Solution_Test
         int output = solution.MyAtoi(input);
         output.Should().Be(expectedOutput);
     }
+
+    [Fact]
+    public void Fact4()
+    {
+        Solution solution = new Solution();
+        string input = "words and 987";
+        int expectedOutput = 0;
+
+        //<NA> Need to remove the leading white space, then check for a -, then check for integers and cut off the value
+
+        int output = solution.MyAtoi(input);
+        output.Should().Be(expectedOutput);
+    }
+
 }
