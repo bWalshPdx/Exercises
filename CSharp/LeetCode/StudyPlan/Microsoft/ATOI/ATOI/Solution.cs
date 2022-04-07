8d6b8284c7bda9478efa5a1c8a85e59052eaf51b
Brian Walsh <bwalsh@ruby.com>
2022-04-07 12:16:13 -0700

    working on new test case

--- CSharp/LeetCode/StudyPlan/Microsoft/ATOI/ATOI/Solution.cs
+++ CSharp/LeetCode/StudyPlan/Microsoft/ATOI/ATOI/Solution.cs
@@ -9,66 +9,95 @@ public class Solution {
     public int MyAtoi(string input)
     {
         bool getIntegerChars = false;
+        bool isNegative = false;
         StringBuilder sb = new StringBuilder();
         
-
         for (int i = 0; i < input.Length; i++)
         {
-            if (Char.IsWhiteSpace(input[i]))
+            char currentChar = input[i];
+            
+            if (Char.IsWhiteSpace(currentChar))
             {
-                getIntegerChars = false;
-
                 if (getIntegerChars)
                 {
                     break;
                 }
+                
+                getIntegerChars = false;
             }
-            else if (input[i] == '-')
+            else if (currentChar == '-')
             {
                 if (getIntegerChars)
                 {
-                    getIntegerChars = false;
                     break;
                 }
                 else
                 {
-                    getIntegerChars = true;
+                    isNegative = true;
                 }
+                
+                getIntegerChars = !getIntegerChars;
             }
-            else if (Char.IsDigit(input[i]))
+            else if (currentChar == '+')
+            {
+                if (getIntegerChars)
+                {
+                    break;
+                }
+                getIntegerChars = !getIntegerChars;
+                
+                continue;
+            }
+            else if (Char.IsDigit(currentChar))
             {
                 getIntegerChars = true;
+                sb.Append(currentChar);
             }
-            else if (Char.IsLetter(input[i]))
+            else if (!Char.IsDigit(currentChar))
             {
-                getIntegerChars = false;
                 break;
             }
-
-            if (getIntegerChars)
-                sb.Append(input[i]);
+    
         }
 
-        Int32.TryParse(sb.ToString(), out int formattedInteger);
+        int? output = MyToInt(sb.ToString().ToCharArray());
 
-        return formattedInteger;
+        if (output == null)
+        {
+            if (isNegative)
+            {
+                return -2147483648;
+            }
+
+            return 2147483647;
+        }
+
+        if (isNegative)
+            output = output * -1;
+        
+        
+        return output.Value;
     }
 
-    public int MyToInt(char[] input)
+    public int? MyToInt(char[] input)
     {
-        int multiple = 1;
-        int output = 0;
+        double multiple = 1;
+        double output = 0;
         
         foreach (char currentChar in input.Reverse())
         {
             Int32.TryParse(currentChar.ToString(), out int formattedInteger);
             output = (formattedInteger * multiple) + output;
-
+            
             multiple = multiple * 10;
         }
 
-        //Int32.TryParse(input[0].ToString(), out int formattedInteger);
-        return output;
+        if (output > 2147483647)
+        {
+            return null;
+        }
+        
+        return Convert.ToInt32(output);
     }
 }
 
@@ -103,8 +132,6 @@ public class Solution_Test
         Solution solution = new Solution();
         string input = "4193 with words";
         int expectedOutput = 4193;
-        
-        //<NA> Need to remove the leading white space, then check for a -, then check for integers and cut off the value
 
         int output = solution.MyAtoi(input);
         output.Should().Be(expectedOutput);
@@ -117,8 +144,6 @@ public class Solution_Test
         string input = "words and 987";
         int expectedOutput = 0;
 
-        //<NA> Need to remove the leading white space, then check for a -, then check for integers and cut off the value
-
         int output = solution.MyAtoi(input);
         output.Should().Be(expectedOutput);
     }
@@ -130,7 +155,17 @@ public class Solution_Test
         string input = "-91283472332";
         int expectedOutput = -2147483648;
 
-        //<NA> Need to remove the leading white space, then check for a -, then check for integers and cut off the value
+        int output = solution.MyAtoi(input);
+        output.Should().Be(expectedOutput);
+    }
+    
+    [Fact()]
+    public void MyAtoi_Fact6()
+    {
+        throw new NotImplementedException("This needs to fail, just like in leetCode");
+        Solution solution = new Solution();
+        string input = "   +0 123";
+        int expectedOutput = 0;
 
         int output = solution.MyAtoi(input);
         output.Should().Be(expectedOutput);
@@ -143,7 +178,7 @@ public class Solution_Test
         char[] input = new []{'1'};
         int expectedOutput = 1;
 
-        int output = solution.MyToInt(input);
+        int output = solution.MyToInt(input).Value;
         output.Should().Be(expectedOutput);
     }
     
@@ -154,7 +189,7 @@ public class Solution_Test
         char[] input = new []{'1', '1'};
         int expectedOutput = 11;
 
-        int output = solution.MyToInt(input);
+        int output = solution.MyToInt(input).Value;
         output.Should().Be(expectedOutput);
     }
     
@@ -165,7 +200,7 @@ public class Solution_Test
         char[] input = new []{'1', '0', '0', '1'};
         int expectedOutput = 1001;
 
-        int output = solution.MyToInt(input);
+        int output = solution.MyToInt(input).Value;
         output.Should().Be(expectedOutput);
     }
 
@@ -174,11 +209,43 @@ public class Solution_Test
     {
         Solution solution = new Solution();
         string input = "91283472332";
-        int expectedOutput = 2147483647;
+        
+        int? output = solution.MyToInt(input.ToCharArray());
+        output.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task MyToInt_Fact5()
+    {
+        Solution solution = new Solution();
+        string input = "3.14159";
+        int expectedOutput = 3;
 
-        //<NA> Need to remove the leading white space, then check for a -, then check for integers and cut off the value
-
-        int output = solution.MyToInt(input.ToCharArray());
+        int output = solution.MyAtoi(input);
         output.Should().Be(expectedOutput);
     }
+    
+    [Fact]
+    public async Task MyToInt_Fact6()
+    {
+        Solution solution = new Solution();
+        string input = "+1";
+        int expectedOutput = 1;
+
+        int output = solution.MyAtoi(input);
+        output.Should().Be(expectedOutput);
+    }
+    
+    [Fact]
+    public async Task MyToInt_Fact7()
+    {
+        Solution solution = new Solution();
+        string input = "+-12";
+        int expectedOutput = 0;
+
+        int output = solution.MyAtoi(input);
+        output.Should().Be(expectedOutput);
+    }
+    
+    
 }
