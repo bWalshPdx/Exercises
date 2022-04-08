2850beadd7cd4b48eeb4500c4d9aa6a72010ddf8
Brian Walsh <bwalsh@ruby.com>
2022-04-08 10:57:39 -0700

    most basic swap passing

--- CSharp/LeetCode/StudyPlan/Microsoft/ReverseString/ReverseString/Solution.cs
+++ CSharp/LeetCode/StudyPlan/Microsoft/ReverseString/ReverseString/Solution.cs
@@ -7,6 +7,15 @@ namespace ReverseString
     {
         public void ReverseString(char[] s)
         {
+            int arrayCount = s.Length;
+
+            int start = 0;
+            int end = arrayCount - 1;
+            char temp = s[start];
+
+            s[start] = s[end];
+            s[end] = temp;
+
 
         }
     }
@@ -20,6 +29,20 @@ namespace ReverseString
             Solution solution = new Solution();
             char[] input = new[] { 'i' };
             char[] expectedOutput = new[] { 'i' };
+
+
+            solution.ReverseString(input);
+
+            input.Should().BeEquivalentTo(expectedOutput);
+        }
+
+
+        [Fact]
+        public void ReverseString_Fact2()
+        {
+            Solution solution = new Solution();
+            char[] input = new[] { 't', 'o' };
+            char[] expectedOutput = new[] { 'o', 't' };
 
 
             solution.ReverseString(input);
