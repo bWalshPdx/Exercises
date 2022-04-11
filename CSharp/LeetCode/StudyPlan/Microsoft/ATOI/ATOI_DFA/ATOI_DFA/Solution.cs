50e8f4385b9a141b85a320c9be9c20779f7767bb
Brian Walsh <bwalsh@ruby.com>
2022-04-11 10:05:38 -0700

    working on start state

--- CSharp/LeetCode/StudyPlan/Microsoft/ATOI/ATOI_DFA/ATOI_DFA/Solution.cs
+++ CSharp/LeetCode/StudyPlan/Microsoft/ATOI/ATOI_DFA/ATOI_DFA/Solution.cs
@@ -1,13 +1,44 @@
-﻿using FluentAssertions;
+﻿using System.Text;
+using FluentAssertions;
 using Xunit;
 
 namespace ATOI_DFA;
 
+public class State
+{
+    public String Input;
+    public StringBuilder StringBuilder;
+    public int Index;
+
+    
+    
+    public State(String input, int index)
+    {
+        StringBuilder = new StringBuilder();
+        Input = input;
+        Index = index;
+    }
+}
+
+public enum DFA_State
+{
+    End
+
+}
+
 public class Solution
 {
+    
+
     public int MyAtoi(string input)
     {
         return 0;
+    }
+
+
+    public State StartingState(State stateInput)
+    {
+        return stateInput;
     }
 }
 
@@ -15,15 +46,18 @@ public class Solution
 
 public class Solution_Tests
 {
+    //TODO: Hard code the state expected output rather than just returning the input:
     [Fact]
-    public async Task Bootstrap()
+    public async Task StartingState_HandleSingleWhitespace()
     {
         Solution solution = new Solution();
-        string input = "";
-        int expectedOutput = 0;
+        
+        State stateInput = new State("", 0);
+        //State stateExpectedOutput = new State("", 0);
+        State stateExpectedOutput = stateInput;
+        
+        State output = solution.StartingState(stateInput);
 
-        int output = solution.MyAtoi("");
-
-        output.Should().Be(expectedOutput);
+        output.Should().Be(stateExpectedOutput);
     }
 }
