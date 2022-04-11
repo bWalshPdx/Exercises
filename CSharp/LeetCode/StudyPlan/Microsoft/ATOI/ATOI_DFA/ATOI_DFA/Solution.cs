bf9dc4b3c9b3396972eb44bcbf0eaa99350a7ed0
Brian Walsh <bwalsh@ruby.com>
2022-04-11 09:42:32 -0700

    Adding Bootstrap working

---
+++ CSharp/LeetCode/StudyPlan/Microsoft/ATOI/ATOI_DFA/ATOI_DFA/Solution.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ATOI_DFA;
+
+public class Solution
+{
+    public int MyAtoi(string input)
+    {
+        return 0;
+    }
+}
+
+
+
+public class Solution_Tests
+{
+    [Fact]
+    public async Task Bootstrap()
+    {
+        Solution solution = new Solution();
+        string input = "";
+        int expectedOutput = 0;
+
+        int output = solution.MyAtoi("");
+
+        output.Should().Be(expectedOutput);
+    }
+}
