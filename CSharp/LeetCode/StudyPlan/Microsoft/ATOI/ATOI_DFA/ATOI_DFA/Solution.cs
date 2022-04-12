801ce9b9913d308b8bad7fbaacb88fe71c091c18
Brian Walsh <bwalsh@ruby.com>
2022-04-12 12:01:12 -0700

    working out how it will go from state to state

--- CSharp/LeetCode/StudyPlan/Microsoft/ATOI/ATOI_DFA/ATOI_DFA/Solution.cs
+++ CSharp/LeetCode/StudyPlan/Microsoft/ATOI/ATOI_DFA/ATOI_DFA/Solution.cs
@@ -6,42 +6,59 @@ namespace ATOI_DFA;
 
 public class State
 {
-    public String Input;
-    public StringBuilder StringBuilder;
-    public int Index;
+    public string Input { get; set; }
+    public StringBuilder StringBuilder { get; set; }
+    public int Index { get; set; }
+    public DFA_State NextState { get; set; }
 
+    public State()
+    {
+    }
     
-    
-    public State(String input, int index)
+    public State(String input)
     {
         StringBuilder = new StringBuilder();
-        Input = input;
-        Index = index;
+        Input = "";
+        NextState = DFA_State.Start;
     }
 }
 
 public enum DFA_State
 {
+    Start,
     End
-
 }
 
 public class Solution
 {
-    
-
     public int MyAtoi(string input)
     {
         return 0;
     }
-
-
+    
     public State StartingState(State stateInput)
     {
-        return stateInput;
+        char[] inputChars = stateInput.Input.ToCharArray();
+        //Handle Whitespace:
+
+        if (inputChars.Length < stateInput.Index)
+            stateInput.NextState = DFA_State.End;
+        else if (inputChars[stateInput.Index] == ' ')
+            stateInput.NextState = DFA_State.Start;
+
+
+        throw new NotImplementedException("Write up the next state logic with the index increment");
+
+
+        return new State()
+        {
+            Input = stateInput.Input,
+            StringBuilder = new StringBuilder(),
+            Index = 1,
+            NextState = DFA_State.End
+        };
     }
 }
-
 
 
 public class Solution_Tests
@@ -51,13 +68,40 @@ public class Solution_Tests
     public async Task StartingState_HandleSingleWhitespace()
     {
         Solution solution = new Solution();
+        string input = "";
         
-        State stateInput = new State("", 0);
-        //State stateExpectedOutput = new State("", 0);
-        State stateExpectedOutput = stateInput;
+        State stateInput = new State("");
+        State stateExpectedOutput = new State()
+        {
+            Input = input,
+            StringBuilder = new StringBuilder(),
+            Index = 1,
+            NextState = DFA_State.End
+        };
         
         State output = solution.StartingState(stateInput);
 
-        output.Should().Be(stateExpectedOutput);
+        output.Should().BeEquivalentTo(stateExpectedOutput);
+    }
+
+
+    [Fact]
+    public async Task StartingState_HandleMultipleWhitespaces()
+    {
+        Solution solution = new Solution();
+        string input = " ";
+
+        State stateInput = new State(input);
+        State stateExpectedOutput = new State()
+        {
+            Input = input,
+            StringBuilder = new StringBuilder(),
+            Index = 1,
+            NextState = DFA_State.Start
+        };
+
+        State output = solution.StartingState(stateInput);
+
+        output.Should().BeEquivalentTo(stateExpectedOutput);
     }
 }
---
+++ CSharp/LeetCode/StudyPlan/Microsoft/ATOI/ATOI_DFA/README.md
@@ -0,0 +1,24 @@
+https://leetcode.com/problems/string-to-integer-atoi/solution/
+
+
+## Reflection:
+
+### What are the best aspects of the design of the code we’ve ended up with?
+
+
+### In what ways did we do Test Driven Development particularly well?
+
+
+### Did we learn anything new?
+
+
+### Did anything unexpected happen?
+
+
+### What do we still need to practice more?
+
+
+### What should we do differently in the next dojo?
+
+
+### What will you do differently tomorrow in your production code?
