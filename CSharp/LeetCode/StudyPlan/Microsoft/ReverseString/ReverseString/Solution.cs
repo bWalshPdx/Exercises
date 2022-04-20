20b0b48789452866780f3419dbff3ef67ba102a1
Brian Walsh <bwalsh@ruby.com>
2022-04-20 11:12:21 -0700

    HOLE IN ONE (First Submittal Passed) Answer accpeted

--- CSharp/LeetCode/StudyPlan/Microsoft/ReverseString/ReverseString/Solution.cs
+++ CSharp/LeetCode/StudyPlan/Microsoft/ReverseString/ReverseString/Solution.cs
@@ -11,10 +11,17 @@ namespace ReverseString
 
             int start = 0;
             int end = arrayCount - 1;
-            char temp = s[start];
 
-            s[start] = s[end];
-            s[end] = temp;
+            while (start < end)
+            {
+                char temp = s[start];
+
+                s[start] = s[end];
+                s[end] = temp;
+
+                start++;
+                end--;
+            }   
         }
     }
 
@@ -54,6 +61,43 @@ namespace ReverseString
             char[] input = new[] { 'f', 'o', 'u', 'r' };
             char[] expectedOutput = new[] { 'r', 'u', 'o', 'f' };
             
+            solution.ReverseString(input);
+
+            input.Should().BeEquivalentTo(expectedOutput, options => options.WithStrictOrdering());
+        }
+
+        [Fact]
+        public void ReverseString_MyFact4()
+        {
+            Solution solution = new Solution();
+            char[] input = new[] { 't', 'w', 'o'};
+            char[] expectedOutput = new[] { 'o', 'w', 't' };
+
+            solution.ReverseString(input);
+
+            input.Should().BeEquivalentTo(expectedOutput, options => options.WithStrictOrdering());
+        }
+
+
+        [Fact]
+        public void ReverseString_MyFact5()
+        {
+            Solution solution = new Solution();
+            char[] input = new[] { 'I'};
+            char[] expectedOutput = new[] { 'I' };
+
+            solution.ReverseString(input);
+
+            input.Should().BeEquivalentTo(expectedOutput, options => options.WithStrictOrdering());
+        }
+
+        [Fact]
+        public void ReverseString_MyFact6()
+        {
+            Solution solution = new Solution();
+            char[] input = { };
+            char[] expectedOutput = {};
+
             solution.ReverseString(input);
 
             input.Should().BeEquivalentTo(expectedOutput, options => options.WithStrictOrdering());
