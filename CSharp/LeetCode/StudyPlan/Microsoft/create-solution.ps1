be1138bd69f8be2fcaf3b4fb833b6df95b2fab2c
Brian Walsh <bwalsh@ruby.com>
2022-05-05 10:58:58 -0700

    initial test passing

--- CSharp/LeetCode/StudyPlan/Microsoft/create-solution.ps1
+++ CSharp/LeetCode/StudyPlan/Microsoft/create-solution.ps1
@@ -1,9 +1,88 @@
-$solutionName = "LeetCodeTemplate"
+param(
+    [string]$solutionName,
+    [string]$solutionLink
+    )
+
+if($solutionName.Length -eq 0) 
+{
+    throw "Solution Name is null"
+}
+
+
+if($solutionLink.Length -eq 0)
+{
+    throw "Solution Link is null"
+}
+
+$classContents = "
+using FluentAssertions;
+using Xunit;
+namespace LeetCodeTemplate;
+
+public class Solution
+{
+
+}
+
+public class Solution_Test
+{
+    [Fact]
+    public void Boilerplate()
+    {
+        Solution s = new Solution();
+        s.Should().NotBeNull();
+    }
+}
+"
+
+$readme = "
+# $solutionName
+-------------------------
+Leetcode description:
+$solutionLink
+
+## Prepping for writing the Solution
+
+### Rules of the Problem:
+
+### Proposed tests for each rule:
+
+## Reflection:
+### What are the best aspects of the design of the code I’ve ended up with?
+
+
+### In what ways did I do Test Driven Development particularly well?
+
+
+### Did I learn anything new?
+
+
+### Did anything unexpected happen?
+
+
+### What do I still need to practice more?
+
+
+### What should you do differently in the next exercise?
+"
+
 
 dotnet new sln -o $solutionName
 
 dotnet new classlib -o $solutionName
-dotnet sln $solutionName.sln add .\$solutionName\$solutionName.csproj
 
-# dotnet add .\MyWebApp.Client\MyWebApp.Client.csproj reference .\MyWebApp.DataStore\MyWebApp.DataStore.csproj
-# dotnet add .\MyWebApp.DataStoreTest\MyWebApp.DataStoreTest.csproj reference .\MyWebApp.DataStore\MyWebApp.DataStore.csproj
+dotnet sln $solutionName\$solutionName.sln add .\$solutionName\$solutionName.csproj
+
+cd $solutionName
+
+
+dotnet add package xunit
+dotnet add package FluentAssertions
+dotnet add package Microsoft.NET.Test.Sdk
+
+echo $classContents > Class.cs
+rm Class1.cs
+
+echo $readme > README.md
+
+cd ..
