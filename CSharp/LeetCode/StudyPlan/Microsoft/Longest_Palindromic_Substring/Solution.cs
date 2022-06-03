495f6c394716a0d676e53411775bb3ad7c036e47
Brian.Walsh <brian.walsh@ruby.com>
2022-06-03 15:12:58 -0700

    working on substring problem

--- CSharp/LeetCode/StudyPlan/Microsoft/Longest_Palindromic_Substring/Solution.cs
+++ CSharp/LeetCode/StudyPlan/Microsoft/Longest_Palindromic_Substring/Solution.cs
@@ -6,6 +6,16 @@ namespace LeetCodeTemplate;
 public class Solution {
     public string LongestPalindrome(string s)
     {
+        for (int index = 0; index < s.Length - 1; index++)
+        {
+            while (true)
+            {
+                
+            }
+            
+        }
+        
+        
         return s;
     }
 
@@ -16,6 +26,16 @@ public class Solution {
         string reversed = new string( charArray );
         
         return reversed == input;
+    }
+
+    public string GetStubstring(List<string> input)
+    {
+        return "333";
+    }
+
+    public string GetStubString(int start, int end, string input)
+    {
+        return "333";
     }
 }
 
@@ -49,6 +69,27 @@ public class Solution_Test
         bool expectedOutput = false;
 
         s.IsPalindrome(input).Should().Be(expectedOutput);
+    }
+    
+    [Fact]
+    public void GetStubString_ShouldReturn333()
+    {
+        Solution s = new Solution();
+        string input = "xxx333x";
+        
+        string output = s.GetStubString(3, 5, input);
+
+        output.Should().Be("333");
+    }
+    
+    
+    [Fact()]
+    public void GetLongestString_ShouldReturn4444()
+    {
+        Solution s = new Solution();
+        string input = "xxx4444x";
+        
+        s.GetStubString(3, 5, input).Should().Be("4444");
     }
 }
 
